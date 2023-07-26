bd20fbf5e5ad1e51fe3dc3336672ef1cbf58b274
Mordzifah99 <mordzifahbenedicta99@gmail.com>
2023-07-25 20:48:41 -0700

    just started

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void reverse(int *array, int size);
+
+#endif
---
+++ task_0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of our program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int value = 5;
+
+	if (value > 5)
+	{
+		printf("you have passed");
+	}
+	else
+	{
+		printf("you have failed");
+	}
+
+	return (0);
+}
